41ccd5cfef2aba6b4342079b2ae09c0f0ef47f88
David Craven <davidcraven@Davids-MacBook-Pro-2.local>
2016-11-26 16:38:18 -1000

    saveload and canvas border check divs added

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -19,6 +19,33 @@ function pixelPainterModule(width,height){
     }
   }
 
+  //blank divs
+  var leftDiv = document.createElement('div');
+  leftDiv.id = 'left';
+  leftDiv.className = 'pp-empty';
+  pixelPainter.appendChild(leftDiv);
+  leftDiv.addEventListener('mouseover', endDraw);
+
+  var topDiv = document.createElement('div');
+  topDiv.id = 'top';
+  topDiv.className = 'pp-empty';
+  pixelPainter.appendChild(topDiv);
+  topDiv.addEventListener('mouseover', endDraw);
+
+
+  var bottomDiv = document.createElement('div');
+  bottomDiv.id = 'bottom';
+  bottomDiv.className = 'pp-empty';
+  pixelPainter.appendChild(bottomDiv);
+  bottomDiv.addEventListener('mouseover', endDraw);
+
+  var rightDiv = document.createElement('div');
+  rightDiv.id = 'right';
+  rightDiv.className = 'pp-empty';
+  pixelPainter.appendChild(rightDiv);
+  rightDiv.addEventListener('mouseover', endDraw);
+
+
   //color palette
   var currentColor;
   var colorArray = ['000000', 'FF0000', 'FF2900', 'FF5200', 'FF7C00', 'FFA500', 'FFBC00', 'FFD200', 'FFE800', 'FFFF00', 'C0E000', '80C000', '40A000', '008000', '408040', '004080', '0020C0', '0000FF', '2000E0', '4000C0', '6000A0', '800080', 'A00060', 'C00040', 'E00020'];
@@ -65,16 +92,35 @@ function pixelPainterModule(width,height){
       currentColor = 'initial';
     });
 
+  // save button
+  var saveButton = document.createElement('button');
+    saveButton.className = 'pp-button';
+    saveButton.id = 'save';
+    pixelPainter.appendChild(saveButton);
+    saveButton.addEventListener('click',saveCanvas);
+
+  // load button
+  var loadButton = document.createElement('button');
+    loadButton.className = 'pp-button';
+    loadButton.id = 'load';
+    pixelPainter.appendChild(loadButton);
+    loadButton.addEventListener('click',loadCanvas);
+
   //fill button
-  var fillButton = document.createElement('button');
-    fillButton.className = 'pp-button';
-    fillButton.id = 'fill';
-    pixelPainter.appendChild(fillButton);
+  // var fillButton = document.createElement('button');
+  //   fillButton.className = 'pp-button';
+  //   fillButton.id = 'fill';
+  //   pixelPainter.appendChild(fillButton);
 
   var clearAudio = document.createElement('audio');
     clearAudio.id = 'clearAudio';
     clearAudio.src = '/imgs/We%20dont%20make%20mistakes%20here....mp3';
     pixelPainter.appendChild(clearAudio);
+
+   var rectAudio = document.createElement('audio');
+    rectAudio.id = 'rectAudio';
+    rectAudio.src = '/imgs/Shoot%20lets%20get%20crazy.mp3';
+    pixelPainter.appendChild(rectAudio);
 
   //functions
   var cellList = ppCanvas.getElementsByTagName('td');
@@ -91,6 +137,8 @@ function pixelPainterModule(width,height){
 
   function rectTool(){
     clearListeners();
+    var audio = document.getElementById("rectAudio");
+    audio.play();
     for (let i = 0; i < cellList.length; i++) {
       cellList[i].addEventListener('mousedown', startRect);
       cellList[i].addEventListener('mouseup', endRect);
@@ -127,6 +175,39 @@ function pixelPainterModule(width,height){
       audio.play();
     for (let i = 0; i < cellList.length; i++) {
       cellList[i].style.backgroundColor = 'initial';
+    }
+  }
+
+  var cellColor = 'initial';
+  var cellDataX = 0;
+  var cellDataY = 0;
+  var cellColorArray = [];
+  var cellDataXArray = [];
+  var cellDataYArray = [];
+
+  function saveCanvas(){
+    cellColorArray = [];
+    cellDataXArray = [];
+    cellDataYArray = [];
+    for (let i = 0; i < cellList.length; i++) {
+      if (cellList[i].style.backgroundColor !== 'initial' && cellList[i].style.backgroundColor !== ''){
+        cellColor = cellList[i].style.backgroundColor;
+        cellDataX = cellList[i].dataX;
+        cellDataY = cellList[i].dataY;
+        cellColorArray.push(cellColor);
+        cellDataXArray.push(cellDataX);
+        cellDataYArray.push(cellDataY);
+      }
+    }
+  }
+
+  function loadCanvas(){
+    for (let i = 0; i < cellList.length; i++) {
+      for (let x = 0; x < cellDataXArray.length; x++) {
+        if (cellList[i].dataX === cellDataXArray[x] && cellList[i].dataY === cellDataYArray[x]){
+          cellList[i].style.backgroundColor = cellColorArray[x];
+        }
+      }
     }
   }
 
